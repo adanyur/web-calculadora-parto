8f56ef35bb47f1a9401fa27d9068a53044d9c738
Yur Adan Valdez Carazas <yvaldez@clinicasantaisabel.com>
2020-12-16 16:26:26 -0500

    se agrego mensaje de resultado

--- js/app.js
+++ js/app.js
@@ -1,61 +1,73 @@
-$(document).ready((e) => {
-  const listaDias = () => {
+$(document).ready(() => {
+  const listDay = () => {
     for (let i = 1; i <= 31; i++) {
       template = `<option value=${i}>${i}</option`;
-      $("#dia").append(template);
+      $("#day").append(template);
     }
   };
-  listaDias();
 
-  const listaMeses = () => {
+  const listMonth = () => {
     $.getJSON("json/meses.json", (data) => {
       for (let meses in data) {
         template = `<option value=${data[meses].value}>${data[meses].descripcion}</option>`;
-        $("#mes").append(template);
+        $("#month").append(template);
       }
     });
   };
 
-  listaMeses();
+  listDay();
+  listMonth();
 
-  $(".button").on("click", (e) => {
-    const dia = $("#dia").val();
-    const mes = $("#mes").val();
-    const anio = $("#anio").val();
+  const DateBirth = (date) => date.setDate(date.getDate() + 280); //FECHA ESTIMADO DEL PARTO
+  const Days = (time) => Math.floor(time / (1000 * 60 * 60 * 24)); //DIAS TRANSCURRIDO
+  const Weeks = (time) => Math.floor(time / (1000 * 60 * 60 * 24) / 7); //NUMERO DE SEMANAS
 
-    let fecha = new Date(anio + "/" + mes + "/" + dia.padStart(2, "0"));
+  const weatherGestation = (date) => {
+    //CALCULA LA SEMANAS Y DIAS
+    const time = new Date() - date;
+    let day = Days(time);
+    let week = Weeks(time);
 
-    fecha = new Date(day(fecha));
-    fecha = new Date(month(fecha));
-    fecha = new Date(year(fecha));
+    // day >= 15 ? new Date().getDate() - day :
 
-    mensaje(fecha);
+    a = `Tienes ${week !== 0 ? week : 0} semanas y ${day} dias de embarazo`;
+    b = `Su bebe se encuentra en sus brazos Felicidades!`;
 
-    e.preventDefault();
-  });
-
-  const day = (fecha) => {
-    return fecha.setDate(fecha.getDate() + 7);
-  };
-  const month = (fecha) => {
-    return fecha.setMonth(fecha.getMonth() - 3);
-  };
-  const year = (fecha) => {
-    return fecha.setYear(fecha.getFullYear() + 1);
+    return `
+        <div class="card-text-ma">
+          ${week >= 40 ? b : a}
+        </div>
+    `;
   };
 
-  const mensaje = (fecha) => {
+  const calculateDate = (date) => {
+    let time = weatherGestation(date);
+    let dateBirth = new Date(DateBirth(date));
+
     let options = { year: "numeric", month: "long", day: "numeric" };
-
-    let template = `<div class="card">
+    let template = `
+        <img src="img/img2.png" class="img"> 
           <div class="card-title">
             Tu bebé nacerá aproximadamente el…
           </div>
+          
           <div class="card-text-ma">
-            ${fecha.toLocaleDateString("default", options)}
+            ${dateBirth.toLocaleDateString("default", options)}
           </div>
-          </div>`;
+          ${time}
+          
+          `;
 
-    $("#mensaje").html(template);
+    $("#message").html(template);
   };
+
+  $(".button").on("click", (e) => {
+    const day = $("#day").val();
+    const month = $("#month").val();
+    const year = $("#year").val();
+
+    let date = new Date(year + "/" + month + "/" + day.padStart(2, "0"));
+    calculateDate(date);
+    e.preventDefault();
+  });
 });
